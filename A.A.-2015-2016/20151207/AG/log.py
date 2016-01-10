515ef20f47f4087d2f7cd59bfbf9a0e91b702454
Andrea Geremy <andreageremia3@gmail.com>
2016-01-10 14:04:01 +0100

    materiali personali aggiunti

--- A.A.-2015-2016/20151207/AG/log.py
+++ A.A.-2015-2016/20151207/AG/log.py
@@ -8,16 +8,16 @@ p0= 0.96 # rapporto tra le due funzioni dur e step
 p1= 0.01
 t0= 0
 t1= ftime
-aw= (math.exp(w0)-math.exp(w1))/(t1-t0) # coeff ang funzione
-bw= math.exp(w1)-(aw*t0) 
-ap= (math.exp(p0)-math.exp(p1))/(t1-t0) 
-bp= math.exp(p1)-(ap*t0) 
+aw= (math.exp(w0)-math.exp(w1))/(t0-t1) # coeff ang funzione
+bw= math.exp(w0)-(aw*t0) 
+ap= (math.exp(p0)-math.exp(p1))/(t0-t1) 
+bp= math.exp(p0)-(ap*t0) 
 at= t0
 while (at < ftime):
 	w= (math.log(aw*at+bw)) # prima funzione lineare (larghezza della finestra) 
 	p= (math.log(ap*at+bw)) # seconda f sul rapporto tra dur e step (percentuale tra dur e step) 
-	step= w*p
-	dur= w-step
+	step= w
+	dur= (w*p)-step #n.a: ho dovuto modificare le formule della durata e dello step dato che, per una questione di calcoli, avevo uno step<0 e dunque non avevo uno step (non si sentivano nella traccia gli stacchi).
 	print "i1 %8.4f %8.4f %8.4f" % (at, dur, at) # %8.4f argomenti che vengono sost nella stringa
 	at= at+w
 print "i1 %8.4f %8.4f %8.4f" % (at, sfdur-ftime, at) # %8.4f argomenti che vengono sost nella stringa

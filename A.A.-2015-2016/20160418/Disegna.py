c071a5950df67556a9140d26ded9945e10d032c3
Nicola Bernardini <nicb@sme-ccppd.org>
2016-05-04 10:38:31 +0200

    added lesson e consegna 20160502

--- A.A.-2015-2016/20160418/Disegna.py
+++ A.A.-2015-2016/20160418/Disegna.py
@@ -4,31 +4,32 @@ width= 50.0
 height= 36.0
 dur= 80.0
 fmax= 6000.0
+fmin= 280.0
 tconv= width/dur
-fconv= height/fmax
-base= 2*fconv 
+fconv= height/(fmax-fmin)
+base= 0.915
 def pow2(value):
 	return math.pow(base, value)
 def log2(value):
-	return math.log(value+0.00001)/math.log(base) 
-fmaxcm= height
-fmincm= fconv
+	return -math.log(value+0.00001)/math.log(base) 
+fmaxcm= height*fconv
+fmincm= fmin*fconv
 afact= (pow2(fmaxcm)-pow2(fmincm))/(fmaxcm)
 bfact= pow2(fmaxcm)-(afact*fmaxcm)
 def geom(freq):
 	return log2(afact*freq+bfact)
 def limit0(value):
 	res= value
-	if value<0.0:
-		res= 0.0
+        if value<fmin:
+		res= fmin
 	return res
 print ".PS\nscale=2.54\nFrame: box width %4.1f height %4.1f invis" % (width, height)
 print "Freq: arrow from Frame.sw to Frame.nw + (0, 1.5)"
 print "Time: arrow from Frame.sw to Frame.se + (1.5, 0)"
 TimeTic= 5.0
-FreqTic= 300.0
+FreqTic= 500.0
 t=0
-f=0
+f=fmin
 while (t <= dur):
         tt=t*tconv
         print "L%d: line from Time.w + (%8.4f, 0) to Time.w + (%8.4f, -0.3)" % (int(tt), tt, tt)
---
+++ A.A.-2015-2016/20160502/README.md
@@ -0,0 +1,16 @@
+# Lezione di lunedì 2 maggio 2016
+
+## Argomenti
+
+* Riflessioni sull'andamento del corso e sui contenuti
+* Riflessione sulla composizione *programmatica* con campioni
+
+## Consegna
+
+* Studiare:
+  * https://www.freesound.org e la sua API in `python`
+  * studiare i descrittori sonori dell'audio
+  * scegliere un inno da realizzare in modalità *suono-mosaico* (come i foto
+    mosaici)
+  * scegliere altre figure retoriche (ad es. *mare*, *esplosione*, *freccia*,
+    ecc.) da realizzare con collezione di campioni

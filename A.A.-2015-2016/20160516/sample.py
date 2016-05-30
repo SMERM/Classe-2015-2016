dfbc0a75f185d96d43eb107c2155a0cf66bac0f3
Andrea Geremy <andreageremia3@gmail.com>
2016-05-30 19:04:36 +0200

    materiali nuova lezione

--- A.A.-2015-2016/20160516/sample.py
+++ A.A.-2015-2016/20160516/sample.py
@@ -9,16 +9,19 @@ Eb= 261.6*(2**(3.0/12.0))
 
 c = freesound.FreesoundClient()
 c.set_token(API_KEY,"token")
+#filter="duration:[0.01 TO %8.4f]&descriptors_filter=lowlevel.pitch.mean:[%8.4f TO %8.4f]"  % (semiminima,G3*0.95, G3*1.05) 
 filter="duration:[0.01 TO %8.4f]" % (semiminima)
-fields="id,name,analysis&descriptors=lowlevel.spectral_centroid.mean,lowlevel.pitch.mean"
-results = c.text_search(query="sci-fi,alien",filter=filter,fields=fields)
+fields="id,name,duration,analysis&descriptors=lowlevel.spectral_centroid,lowlevel.pitch.mean"
+query= "sci-fi"
+results = c.text_search(query=query,filter=filter,fields=fields)
 print results.count
 page = 15
 npages = results.count/page
 p = 1
 while (p<=npages):
-        results = c.text_search(query="sci-fi,alien",filter=filter,fields=fields,page=p)
+        results = c.text_search(query=query,filter=filter,fields=fields,page=p)
         for sound in results:
                 print(sound.as_dict())
+                
         p = p+1
 
